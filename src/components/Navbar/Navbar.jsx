5c9bfa8743f055a73c49fa221ce5cb57cfa1b6db
NicoC0 <nicoconfaa@gmail.com>
2022-04-25 17:39:26 -0300

    add navbar and project structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav className='app__navbar'>
+      <ul className='app__navbar-links'>
+        <li><a href="#proyecto">Proyecto</a></li>
+        <li><a href="#paper">Paper</a></li>
+        <li><a href="#casos">Casos-Videos</a></li>
+        <li><a href="#organizaciones">Organizaciones</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
