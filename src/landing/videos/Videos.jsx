6a581e6e1f0b26b8aa1ecc18dfbf184431cde5ca
NicoC0 <nicoconfaa@gmail.com>
2022-05-06 13:28:57 -0300

    video added

--- src/landing/videos/Videos.jsx
+++ src/landing/videos/Videos.jsx
@@ -1,5 +1,5 @@
 import "./Videos.scss"
-
+import videoIcon from '../../assets/iconos/iconoVideo.svg'
 const Videos = () => {
   return (
     <div id="casos" className="videos__container">
@@ -7,6 +7,13 @@ const Videos = () => {
         <h2 className='videos__title--pink'>#datos<span>en</span>fuga #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga #datos<span>en</span>fuga</h2>
         <h2 className='videos__title--white'>#datos<span>en</span>fuga #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga #datos<span>en</span>fuga</h2>
         <h2 className='videos__title--green'>#datos<span>en</span>fuga #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga  #datos<span>en</span>fuga #datos<span>en</span>fuga</h2>
+      </div>
+      <div className='videos__box'>
+        <iframe width="806" height="456" src="https://www.youtube.com/embed/FIMkE6u9pqs" title="Datos en fuga" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <button className="videos__button">
+          <img src={videoIcon} alt="download"/>
+          <span>Conoce otros casos como este</span>
+        </button>
       </div>
     </div>
   )
