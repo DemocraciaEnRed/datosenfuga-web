2b08e57f577edc730e1b4b60243b342a1b4af0c6
NicoC0 <nicoconfaa@gmail.com>
2022-06-22 12:28:28 -0300

    hardcoded faqs for now

--- src/components/Muestra/Faq/Faq.jsx
+++ src/components/Muestra/Faq/Faq.jsx
@@ -1,5 +1,6 @@
 import "./Faq.scss";
-import { faqs } from "./textFaqs"
+// import { faqs } from "./textFaqs"
+
 const Faq = () => {
   const handleClick = (e) => {
     e.stopPropagation();
@@ -16,20 +17,61 @@ const Faq = () => {
     <div id="faqMuestra" className="faq">
       <div className="faq__wrapper">
         <p className="faq__title">Preguntas frecuentes</p>
-        {
-          faqs.map(faq => {
-            return (
-              <div className="faq__box" key={faq.id}>
-                <button className="faq__accordion" onClick={handleClick}>
-                  {faq.title}
-                </button>
-                <div className="faq__panel">
-                  <p>{faq.text}</p>
-                </div>
-              </div>
-            )
-          })
-        }
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+            1. ¿Quiénes se pueden presentar a esta convocatoria?
+          </button>
+          <div className="faq__panel">
+            <p>Pueden presentarse artistas, investigadores, tecnólogos y todos aquellos que desarrollen proyectos en torno a la relación arte, tecnología y sociedad vinculados al eje curatorial de esta exhibición de cualquier parte del territorio argentino sin limitaciones de edad.</p>
+          </div>
+        </div>
+
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+            2. ¿Qué tengo que presentar para aplicar?
+          </button>
+          <div className="faq__panel">
+            <p>En el <a href="https://forms.gle/y8WvKC7fWnGnc5x19" target="_blank" rel="noreferrer">formulario</a> de aplicación deberás completar con información personal, datos de contacto y la información específica de tu propuesta explicitando la relación con el eje curatorial de la exhibición.</p>
+          </div>
+        </div>
+
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+            3. ¿Puedo postular más de una obra?
+          </button>
+          <div className="faq__panel">
+            <p>Sí, podés postular todas las obras que desees ya sean en carácter individual o colectivo, utilizando un formulario para cada una de las obras.</p>
+          </div>
+        </div>
+
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+            4. ¿Hasta cuándo se pueden enviar propuestas?
+          </button>
+          <div className="faq__panel">
+            <p>Hasta el 10/07/202 de 2022 a las 23:59h (GMT-3)</p>
+          </div>
+        </div>
+
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+            5. ¿Cómo se realiza la inscripción?
+          </button>
+          <div className="faq__panel">
+            <p>Todas las postulaciones se realizan a través del <a href="https://forms.gle/y8WvKC7fWnGnc5x19" target="_blank" rel="noreferrer">formulario</a>, no se recibirán propuestas por correo ni por ningún otro medio.</p>
+          </div>
+        </div>
+
+        <div className="faq__box">
+          <button className="faq__accordion" onClick={handleClick}>
+          6. ¿Cuál es el cronograma de la convocatoria?
+          </button>
+          <div className="faq__panel">
+            <p>Convocatoria: del 22 de junio al 10 de julio de 2022</p>
+            <p>Comunicación de seleccionades: 22 de julio de 2022</p>
+            <p>Exhibición: 19 y 20 de agosto de 2022</p>
+          </div>
+        </div>
         <a id="basesCondiciones" href="https://drive.google.com/file/d/1_nsZ417zLXd4hYnFPGRruOnvwBWG4Lz0/view?usp=sharing"><button className="faq__legal">BASES Y CONDICIONES</button></a>
         <div id="contactoMuestra" className="faq__contact">
           <p className="faq__contact__title">Contacto</p>
