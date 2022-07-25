a0c96cbfcee75e351e024374c07f09992c0db467
GerardoDR <gdrodriguezok@gmail.com>
2022-07-25 13:50:50 -0300

    modif muestra- 'postula tu proyecto' + header

--- src/components/Muestra/Muestra.jsx
+++ src/components/Muestra/Muestra.jsx
@@ -1,58 +1,49 @@
 import React from 'react'
 import './Muestra.scss'
-import { logos }  from '../../constants'
+import { logos } from '../../constants'
 import Sticky from './Sticky/Sticky'
 import Faq from './Faq/Faq'
 
 const Muestra = () => {
   return (
     <div className="app__muestra">
-      {/* Header naranja */}
       <div className="app__muestra-header">
         <div className="app__muestra-header_textos">
-          <span id="title-convocatorias">CONVOCAMOS ARTISTAS</span> <br />
+          <span className="title-convocatorias">CONVOCAMOS ARTISTAS</span> <br />
           <span>PARA POP-UP EN AGOSTO</span>
           <p>Te invitamos a inscribirte como artista o participante</p>
         </div>
         <div className="app__muestra-header_lista">
           <ul>
-            <li><img src={logos.convocatoria} alt="Convocatoria"></img> <p>Convocatoria Abierta</p></li>
-            <li><img src={logos.postulaciones} alt="Postulaciones"></img><p>Postulaciones: <b>22/06/2022</b> al<b> 10/07/2022</b></p></li>
-            <li><img src={logos.prorroga} alt="prorroga" /><p><b>PRÓRROGA POSTULACIONES </b>hasta <b>22/07/2022</b></p></li>
-            <li><img src={logos.publicacion} alt="Publicación"></img> <p>Publicación de obras: <b>22/07/2022</b></p> </li>
+            <li><img src={logos.convocatoria} alt="Convocatoria"></img> <p>Convocatoria Cerrada</p></li>
+            <li><img src={logos.publicacion} alt="Publicación"></img> <p className='app__muestra-header_lista_obras'>Próximamente anunciaremos las obras seleccionadas.</p> </li>
             <li><img src={logos.evento} alt="Evento"></img> <p>Evento: <b>19 y 20 de Agosto 2022</b></p> </li>
           </ul>
         </div>
       </div>
-      <Sticky/>
-      {/* Información de la muestra */}
+      <Sticky />
       <div className="app__muestra-contenido">
         <p id="sobreLaMuestra" className="contenido__title">SOBRE LA MUESTRA</p>
         <p className="contenido__text">
           Vivimos rodeados de datos: los utilizamos, los producimos y se los entregamos a un sin fín de aplicaciones y plataformas aún sin darnos cuenta pero sabiendo que el resultado de todo ese conjunto de datos servirá para que nos envíen publicidades cada vez más precisas y específicas sobre nuestros intereses. Pero ¿qué sucede cuando nuestros datos son gestionados por plataformas del Estado? ¿Sabemos qué tan seguros se encuentran nuestros datos personales?
           <br /> <br />
-          Esta experiencia multidisciplinaria invita a observar el vínculo inevitable entre infraestructuras, sistemas de información, plataformas y datos personales a través de una serie de preguntas: ¿quiénes tienen acceso a nuestros datos? ¿cómo funcionan los sistemas de protección de estos sistemas? ¿es posible desde la ciudadanía contribuir a la protección de estos sistemas bajo un marco legal que no penalice a quienes detectan y reportan vulnerabilidades? 
+          Esta experiencia multidisciplinaria invita a observar el vínculo inevitable entre infraestructuras, sistemas de información, plataformas y datos personales a través de una serie de preguntas: ¿quiénes tienen acceso a nuestros datos? ¿cómo funcionan los sistemas de protección de estos sistemas? ¿es posible desde la ciudadanía contribuir a la protección de estos sistemas bajo un marco legal que no penalice a quienes detectan y reportan vulnerabilidades?
           <br /> <br />
-          Datos en Fuga invita a artistas, investigadores, activistas y tecnólogos de Argentina a participar de la exhibición con obras que propongan miradas, metáforas y acciones en relación a estos interrogantes. 
+          Datos en Fuga invita a artistas, investigadores, activistas y tecnólogos de Argentina a participar de la exhibición con obras que propongan miradas, metáforas y acciones en relación a estos interrogantes.
         </p>
-        
-        {/* spacer */}
+
         <div className="spacer_muestra">
           <img src={logos.spacer} alt="" />
         </div>
-        
-        <p id="postulaTuObra"className="contenido__title">POSTULÁ TU OBRA</p>
-        <p className="contenido__postergacion"><b>PRÓRROGA para POSTULACIÓN de obras </b>hasta <b>22/07/2022</b></p>
-        <p className="contenido__text">
-          Datos en Fuga recibe postulaciones de obras en todos los formatos y medios, siendo algunos ejemplos: 
-          <br /> <br />
-          <b>Instalaciones, objetos digitales, sitios web, visualizaciones de datos, apps, acciones o performance.</b>
-          <br /> <br />
-          Las obras postuladas deberán estar producidas al momento de la aplicación, no se aceptarán proyectos o trabajos en proceso, y les artistas seleccionados contarán con honorarios por su participación, siendo la organización quien cubre los gastos de traslado, montaje, seguros y desmontaje.
+
+        <p id="postulaTuObra" className="contenido__title">POSTULÁ TU OBRA</p>
+        <p className="contenido__convocatoria">
+          <b>La convocatoria esta cerrada</b><br/>
+          <b className="contenido__convocatoria--small">
+            ¡Muchas gracias por tu interés y participación!</b>
         </p>
-        <button className='muestra_button'>
-          <a href="https://forms.gle/CNvzabMjvgZ3TWeDA" target="_blank" rel="noopener noreferrer">Inscribí tu obra</a>
-        </button>
+        <p className="contenido__text">La inscripción de obras se encuentra cerrada, próximamente podrás conocer las obras seleccionadas y mas información sobre ellas!
+        </p>
         {/* spacer
         <div className="spacer_muestra">
           <img src={logos.spacer} alt="" /> 
@@ -71,7 +62,7 @@ const Muestra = () => {
           <a href="https://forms.gle/2TC4MJHvryLK4BDw7" target="_blank" rel="noopener noreferrer">Anotate</a>
         </button> */}
       </div>
-      <Faq/>
+      <Faq />
     </div>
   )
 }
