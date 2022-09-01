4dc8599cae76548d20c0fb2badfd6dd7e4e6caa5
NicoC0 <nicoconfaa@gmail.com>
2022-09-01 20:49:46 -0300

    refactor: added the image of the work to the object

--- src/components/Muestra/Obras/dataObras.js
+++ src/components/Muestra/Obras/dataObras.js
@@ -1,27 +1,33 @@
+import { obrasAgosto } from '../../../assets/obras';
+
 export const obras = [
   {
     title: 'Carpeta Fotos',
     autor: 'Florencia Vallejos',
     description: 'Máquinas que recuerdan y personas que olvidan. ¿Por qué es que solo recuerdo los momentos que tengo filmados? Siempre hay algo que le escapa al encuadre del video, y se almacena en un fuera de campo imaginario, que esconde olores, sensaciones, frases, dudas. ¿Cómo vamos a construir nuestra memoria de archivos virtuales? ¿Dónde se almacenan nuestros recuerdos? El registro digital como anclaje a una verdad, que empieza a fallar, cuando el robot también olvida.',
-    link: 'https://www.instagram.com/florvva/'
+    link: 'https://www.instagram.com/florvva/',
+    img: obrasAgosto.carpetaFotos
   }, 
   {
     title: 'La luz del vacío',
     autor: 'María Florencia Sanchez Aquino',
     description: '"La luz del vacío" es una instalación electrónica que intenta exponer de forma lumínica, una visualización de datos alternativa respecto de aquellas estadísticas que se presentan de forma banalizada en muchos medios de comunicación. Si bien es claro que cada víctima de violencia de género representa más que un dato expresado en una instalación electrónica, es en cada Luz que se enciende en que está obra intenta gritar un continuo #Niunamenos por todas y cada una de las víctimas que hoy no están.',
-    link: 'https://www.instagram.com/flordemanija/?hl=es'
+    link: 'https://www.instagram.com/flordemanija/?hl=es',
+    img: obrasAgosto.luzVacio
   },
   {
     title: 'SELF (de la serie DATA/MOOD/SELF)',
     autor: 'Diego Javier Alberti',
     description: 'Esta obra conforma tres piezas generativas, entramados entre diferentes tipos de datos que producen un hecho estético, una manifestación visual de ese cruce arbitrario o caprichoso que representa el recorte posible de la subjetividad particular de un individuo frente a la complejidad. El proyecto consiste en un sistema de tres obras en torno a la pandemia, como tema que atravesó diferentes esferas de la experiencia. En un ejercicio pensado desde la tríada, se presentan tres órdenes dentro del mismo tema, en representación de las diferentes perspectivas: DATA / MOOD / SELF.',
-    link: 'http://www.diegoalberti.net/'
+    link: 'http://www.diegoalberti.net/',
+    img: obrasAgosto.self
   },
   {
     title: 'El sol es mi pantalla / capturas de la privacidad',
     autor: 'Carla Beatriz Tortul',
     description: 'Es una obra generativa de procesos vinculados con la “memoria digital” desde la investigación biográfica de archivos y la privacidad de datos personales. Expone una revisión de los actos cotidianos, la ética de las relaciones interpersonales, la crisis biopolítica y la carencia de derechos tecnodigitales. ¿El Sol? ¿Cuál sol? Es la retórica que imprime el bordeamiento de una sociedad dormida sin manifestarse, excedida de virtualidad, de pantallas, en la distancia impuesta por lo digital. ¿Qué guarda la memoria digital de un teléfono? Información y recorridos sensibles que actúan como caso de estudio en situaciones sociales trazadas por una curaduría de la vida misma. En estas tensiones se indica un sol que “no” existe, reemplazado por los dispositivos móviles que capturan tiempo, emociones y memorias íntimas, por lo tanto, privadas y vulnerables."',
-    link: 'https://www.instagram.com/carla_tortul/?hl=es'
+    link: 'https://www.instagram.com/carla_tortul/?hl=es',
+    img: obrasAgosto.solPantalla
   },
   {
     title: 'Calle Vista',
@@ -35,7 +41,8 @@ export const obras = [
       linkAutor1: 'https://www.instagram.com/maxparla',
       linkAutor2: 'https://instagram.com/jwanleguizamon',
       linkAutor3: 'https://instagram.com/sofiapazbasail',
-    }
+    },
+    img: obrasAgosto.calleVista
   }
 ]
 
