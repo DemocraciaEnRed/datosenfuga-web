7980e719e8416263795fdcd877155e3f816663f0
NicoC0 <nicoconfaa@gmail.com>
2022-09-01 20:49:01 -0300

    added images corresponding to the works

---
+++ src/assets/obras/index.js
@@ -0,0 +1,8 @@
+import calleVista from './agosto/calle_vista.png'
+import carpetaFotos from './agosto/carpeta_fotos.png'
+import luzVacio from './agosto/luz_del_vacio.jpg'
+import self from './agosto/self.png'
+import solPantalla from './agosto/sol_mi_pantalla.jpg'
+
+const obrasAgosto = {calleVista, carpetaFotos, luzVacio, self, solPantalla}
+export { obrasAgosto }
