8fff295857197ec9cfedeb78d797b849e94738d2
NicoC0 <nicoconfaa@gmail.com>
2022-09-01 20:50:58 -0300

    refactor: rendering of the image of the works

--- src/components/Muestra/Obras/Obras.jsx
+++ src/components/Muestra/Obras/Obras.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { obras } from "./dataObras";
 import './Obras.scss'
 
-const Obras = () => {
+const Obras = ({display}) => {
   return (
     <div className="app__obras">
       <div className="app__obras-container">
@@ -13,6 +13,7 @@ const Obras = () => {
                 <div key={obra.autor+'-'+obra.title}>
                   <p className="obra__title">{obra.title}</p>
                   <p className="obra__autor"><a href={obra.link} target="_blank" rel="noreferrer">{obra.autor}</a></p>
+                  {display === 3 ? <img src={obra.img} alt={obra.title} /> : '' }
                   <p className="obra__descripcion">{obra.description}</p>
                 </div>
               )
