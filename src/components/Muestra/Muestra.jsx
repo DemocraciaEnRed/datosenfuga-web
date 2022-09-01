74d2800a495859bfd85f8fd432f5b5eb01e707cf
NicoC0 <nicoconfaa@gmail.com>
2022-09-01 20:50:13 -0300

    refactor: added variable to render the new section

--- src/components/Muestra/Muestra.jsx
+++ src/components/Muestra/Muestra.jsx
@@ -8,8 +8,8 @@ import Typeform from '../Typeform/Typeform'
 
 const Muestra = () => {
 
-  // 0 postulaciones no habilitadas, 1 habilitadas, 2 expositor muestras
-  let display = 2
+  // 0 postulaciones no habilitadas, 1 habilitadas, 2 expositor muestras, 3 "reviví la muestra"
+  let display = 3
 
   if (display === 1) {
     return (
@@ -207,6 +207,36 @@ const Muestra = () => {
           <Obras />
         </div>
         <Faq/>
+      </div>
+    )
+  } else {
+    return (
+      <div className="app__muestra">
+        {/* Header naranja */}
+        <div className="app__muestra-header-obras">
+          <div className="app__muestra-header-obras_textos">
+            <span className="title-obras">MUESTRA DE ARTE</span> <br />
+            <span className="subtitle-obras">#DATOSENFUGA</span>
+            <p className="text-obras">Un proyecto que reflexiona sobre la privacidad de los datos personales, la gobernanza,  el valor y el uso de los datos, la ciberseguridad y la vida en tiempos de virtualidad. </p>
+          </div>
+        </div>
+        <Sticky/>
+
+        <div className="app__muestra-contenido">
+          <p id="sobreLaMuestra" className="contenido__title">SOBRE LA MUESTRA</p>
+          <p className="contenido__text">
+            Es una experiencia multidisciplinaria que invita a observar el vínculo inevitable entre infraestructuras, sistemas de información, plataformas y datos personales a través de una serie de preguntas: ¿quiénes tienen acceso a nuestros datos? ¿cómo funcionan los sistemas de protección de estos sistemas? ¿es posible desde la ciudadanía contribuir a la protección de estos sistemas bajo un marco legal que no penalice a quienes detectan y reportan vulnerabilidades? 
+            <br /><br />
+            Datos en Fuga invitó a artistas, investigadores, activistas y tecnólogos de Argentina a participar de la exhibición con obras que propongan miradas, metáforas y acciones en relación a estos interrogantes. 
+          </p>
+          
+          <div className="spacer_muestra">
+            <img src={logos.spacer} alt="" /> 
+          </div>
+
+          <p id="eventoObras" className="contenido__title">OBRAS</p>
+          <Obras display={3}/>
+        </div>
       </div>
     )
   }
