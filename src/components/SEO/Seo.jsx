5dfcc9848e013d72166c25ee514715ee84104328
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2023-03-09 11:54:05 -0300

    og:url & twitter:url reflect current path

--- src/components/SEO/Seo.jsx
+++ src/components/SEO/Seo.jsx
@@ -1,24 +1,36 @@
 import { Helmet } from 'react-helmet';
+import { useLocation } from "react-router-dom";
 
 const Seo = ({ data }) => {
 
-    let seoTitle = `${data?.title} | Datos en Fuga` || 'Datos en Fuga | ¿Tu información está segura?'
+    const location = useLocation();
+    const urlBuilder = () => {
+        if(location.hash){
+            if(location.pathname.length > 1){ return (location.pathname + location.hash); }
+            return ('/' + location.hash)
+        }else{
+            return location.pathname
+        }
+    }
+    let seoTitle = data.title? `${data?.title} | Datos en Fuga` : 'Datos en Fuga | ¿Tu información está segura?'
     let seoDescription = data?.description || 'Queremos que el Estado asegure estándares de ciberseguridad y que  deje de perseguir hackers que realizan denuncias.'
     let seoKeywords = data?.keywords || 'Hacker, ciberseguridad, Observatorio de Derecho Informático Argentina, Fundación vía libre, ley de datos personales, ley penal argentina, Incidentes, ciberseguridad, filtración de datos'
+    let seoUrl = urlBuilder()
     
+    console.log(seoUrl)
     return (
         <Helmet>
             <title>{seoTitle}</title>
             <meta name="description" content={seoDescription} />
             <meta name="keywords" content={seoKeywords} />
 
-            <meta property="og:url" content="https://datosenfuga.org/" />
+            <meta property="og:url" content={`https://datosenfuga.org/#${seoUrl}`} />
             <meta property="og:title" content={seoTitle} />
             <meta property="og:description" content={seoDescription} />
 
-            <meta property="twitter:url" content="https://datosenfuga.org/" />
-            <meta property="twitter:title" content={seoTitle} />
-            <meta property="twitter:description" content={seoDescription} />
+            <meta name="twitter:url" content={`https://datosenfuga.org/#${seoUrl}`} />
+            <meta name="twitter:title" content={seoTitle} />
+            <meta name="twitter:description" content={seoDescription} />
         </Helmet>
     )
 }
