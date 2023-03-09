1fbea12a3025304cd54d099f774ecab0e98368b7
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2023-03-09 17:53:27 -0300

    minor tweaks

--- src/components/SEO/Seo.jsx
+++ src/components/SEO/Seo.jsx
@@ -20,7 +20,7 @@ const Seo = ({ data }) => {
         }
     }
 
-    let seoTitle = data.title ? `${data?.title} | Datos en Fuga` : 'Datos en Fuga | ¿Tu información está segura?'
+    let seoTitle = data?.title ? `${data.title} | Datos en Fuga` : 'Datos en Fuga | ¿Tu información está segura?'
     let seoDescription = data?.description || 'Queremos que el Estado asegure estándares de ciberseguridad y que  deje de perseguir hackers que realizan denuncias.'
     let seoKeywords = data?.keywords || 'Hacker, ciberseguridad, Observatorio de Derecho Informático Argentina, Fundación vía libre, ley de datos personales, ley penal argentina, Incidentes, ciberseguridad, filtración de datos'
     let seoUrl = urlBuilder()
